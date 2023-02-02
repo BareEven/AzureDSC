ee5642bddf2e720d9c7fd3a0fa8dc0640be1690d
Even Johannessen <even.johannessen@basefarm.com>
2023-02-02 08:09:28 +0100

    another one

--- Deploy-Bastion/Deploy-Winbast.ps1
+++ Deploy-Bastion/Deploy-Winbast.ps1
@@ -15,10 +15,8 @@ Configuration Deploy-Winbast {
     Import-DscResource -ModuleName 'ActiveDirectoryDsc'
 
     # Create the NetBIOS name and domain credentials based on the domain FQDN
-    [System.Management.Automation.PSCredential] $domainCredential = New-Object System.Management.Automation.PSCredential ("$($domainFQDN)\$($adminCredential.UserName)", $adminCredential.Password)
-    $interface = Get-NetAdapter | Where-Object Name -Like "Ethernet*" | Select-Object -First 1
+    [System.Management.Automation.PSCredential] $domainCredential = New-Object System.Management.Automation.PSCredential ($adminCredential.UserName, $adminCredential.Password)
     &ipconfig /renew
-    $interfaceAlias = $($interface.Name)
 
     Node localhost
     {
@@ -58,29 +56,6 @@ Configuration Deploy-Winbast {
         {
             Name = 'RebootAfterJoiningDomain'
             DependsOn = "[Computer]JoinDomain"
-        }
-    }
-}
-
-function Get-NetBIOSName {
-    [OutputType([string])]
-    param(
-        [string] $domainFQDN
-    )
-
-    if ($domainFQDN.Contains('.')) {
-        $length = $domainFQDN.IndexOf('.')
-        if ( $length -ge 16) {
-            $length = 15
-        }
-        return $domainFQDN.Substring(0, $length)
-    }
-    else {
-        if ($domainFQDN.Length -gt 15) {
-            return $domainFQDN.Substring(0, 15)
-        }
-        else {
-            return $domainFQDN
         }
     }
 }
